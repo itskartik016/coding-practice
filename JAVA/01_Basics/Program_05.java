b2d5c29187aa038051777b20918100ffddd9e0c2
Kartik Jadhav <kartikjadhav.work1@gmail.com>
2026-08-03 23:02:01 +0530

    Add Program_05 to swap two numbers
    Implement a program to swap two numbers using arithmetic operators.

---
+++ JAVA/01_Basics/Program_05.java
@@ -0,0 +1,14 @@
+/*
+    Swap Two Numbers Without Using a Third Variable (Using Arithmetic Operators)
+*/
+
+import java.util.Scanner;
+
+public class Program_05 {
+    public static void main(String args[]){
+
+        Scanner sc = new Scanner (System.in);
+
+        
+    }    
+}
