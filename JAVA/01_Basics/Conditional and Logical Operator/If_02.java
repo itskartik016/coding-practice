f2ef022f57631fb17975bf6c5ffbbf8e31776782
Kartik Jadhav <kartikjadhav.work1@gmail.com>
2026-08-07 00:00:08 +0530

    Create If_02.java

---
+++ JAVA/01_Basics/Conditional and Logical Operator/If_02.java
@@ -0,0 +1,47 @@
+//Print a number only if it is greater than 100.
+
+import java.util.Scanner;
+
+public class If_02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // System.out.println("Enter Number :");
+        // int num = sc.nextInt();
+
+        // if( num > 100){
+        //     System.out.println(num);
+        // } else {
+        //     System.out.println(num + " < " + "100");
+        // }
+
+
+        /*
+            Print "Adult" only if age is at least 18.
+        */
+            // System.out.println("Enter Age:");
+            // int age = sc.nextInt();
+
+            // if(age > 18){
+            //     System.out.println("Adult");
+            // }
+
+        
+        /* 
+            Print "Login Successful" only if isLoggedIn is true.
+        */
+
+            // boolean IsLoggedIn = true;
+
+            // if(IsLoggedIn){
+            //         System.out.println("Login Successful");
+            // }
+
+        /*  
+            Print "Character is Uppercase" if the character is between 'A' and 'Z'.
+        */
+
+            
+
+    }
+}
