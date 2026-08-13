04c251be5118eedd4b7dd0443af066059d2feb73
Kartik Jadhav <kartikjadhav.work1@gmail.com>
2026-08-14 01:10:02 +0530

    Infinite loop

---
+++ JAVA/01_Basics/Loops/While_07.java
@@ -0,0 +1,13 @@
+// infinite loop
+
+import java.util.*;
+
+public class While_07 {
+    public static void main(String[] args) {
+        int i = 1;
+        while(true){                          // always true 
+            System.out.println("KJ" + i);     
+            i++;                           // updates after each time.
+        }
+    }
+}
