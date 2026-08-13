910d0aef41dbf2da32224501148a0ee7a786b6a1
Kartik Jadhav <kartikjadhav.work1@gmail.com>
2026-08-13 23:22:51 +0530

    .

---
+++ JAVA/01_Basics/Loops/While_02.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class While_02{
+    public static void main(String args[]){
+
+        int counter = 1; 
+
+        while( counter <= 10){
+            System.out.println(counter);
+            counter++;                         // updates number
+        }
+    }
+}
