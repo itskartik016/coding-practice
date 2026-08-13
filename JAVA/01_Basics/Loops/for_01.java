4e8c9a9c497b18307cc66390ffdd1dd987e6b1c7
Kartik Jadhav <kartikjadhav.work1@gmail.com>
2026-08-14 00:53:23 +0530

    print HW 10 times.

---
+++ JAVA/01_Basics/Loops/for_01.java
@@ -0,0 +1,12 @@
+// print hello world 10 times using for loop
+
+import java.util.*;
+
+public class for_01 {
+    public static void main(String[] args) {
+    
+        for( int i = 1; i <= 10; i++){
+            System.out.println("Hello World..");
+        }
+    }
+}
