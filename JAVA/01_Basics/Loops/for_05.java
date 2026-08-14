317935f0d4b4daf1f0e0137c36c9e7b5de62c9f6
Kartik Jadhav <kartikjadhav.work1@gmail.com>
2026-08-14 11:54:30 +0530

    Increment and decrement
    Added a new Java file for practicing loops with increment and decrement examples.

---
+++ JAVA/01_Basics/Loops/for_05.java
@@ -0,0 +1,19 @@
+// Basics practice....
+
+import java.util.*;
+
+public class for_05 {
+    public static void main(String[] args) {
+        
+        // INCREMENT
+        // for( int i=1; i<=4; i++ ){
+        //     System.out.println("KJ" + i);
+        // }
+
+        // DECREMENT
+        // for(int i =10; i>=1; i--){
+        //     System.out.println("Kj" + i);
+        // }
+
+    }
+}
