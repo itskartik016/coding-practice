f4984b9a393fbff423e8631072659c6bbf996368
Kartik Jadhav <kartikjadhav.work1@gmail.com>
2026-08-14 11:42:31 +0530

    DoWhile_02.java
    print HELLO 10 times

---
+++ JAVA/01_Basics/Loops/DoWhile_02.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class DoWhile_02 {
+    public static void main(String[] args) {
+        
+        int i = 1;
+
+        do{
+            System.out.println("Hello KJ");
+            i++;
+        } while( i <= 10);
+    }
+}
