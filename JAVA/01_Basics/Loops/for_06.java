073d781d2dba54a2705ed548476e43ac1504d40c
Kartik Jadhav <kartikjadhav.work1@gmail.com>
2026-08-15 00:34:27 +0530

    *** For loop practice ***
    this program includes basic for loop problems and their solutions

---
+++ JAVA/01_Basics/Loops/for_06.java
@@ -0,0 +1,130 @@
+import java.util.*;
+
+public class for_06 {
+    public static void main(String[] args) {
+        
+        // Write a loop that prints 2,4,6,8,10.
+        // for( int i=2; i <= 10; i += 2){
+        //     System.out.println(i);
+        // }
+
+        // print JAVA 5 times.
+        // for( int i=1; i <=5; i++){
+        //     System.out.println("JAVA");  
+        // }
+
+        // Write a Java program that prints the numbers from 1 to 100, but only prints numbers that are divisible by 5.
+
+        // for( int i=1; i <= 100; i++){
+
+        //     if( i % 5 == 0){
+        //         System.out.println(i);
+        //     }
+        // }
+
+        // print all odd numbers from 1 to 20.
+
+        // for( int i=1; i <= 20; i++){
+
+        //     if( i % 2 == 0){
+        //         continue;
+        //     }
+        //     System.out.println(i);
+        // }
+
+
+        // print odd.
+        // for (int i = 1; i <= 20; i += 2) {
+        //     System.out.println(i);
+        // }
+            
+        // Print all even numbers from 1 to 20.
+
+        // for(int i = 0; i <= 20; i +=2){
+        //     System.out.println(i);
+        // }
+
+
+        // Print odd numbers from 1 to 10.
+        // for (int i = 1; i <= 10; i += 2) {
+        //         System.out.println(i);
+        //     }
+
+
+        //Print numbers from 1 to 20 that are divisible by 3.
+
+        // for( int i=1; i <=20; i++){
+            
+        //     if( i % 3 == 0){
+        //         System.out.println(i);
+        //     }
+        // }
+
+        
+        //Print numbers from 1 to 50 that are divisible by both 3 and 5.
+
+        // for( int i=1; i <= 50; i++){
+
+        //     if( i % 3 == 0 && i % 5 ==0){
+        //         System.out.println(i);
+        //     }
+        // }
+
+
+        //Print numbers from 1 to 30 that are not divisible by 2.
+
+        // for ( int i=1; i <= 30; i++){
+
+        //     if( i % 2 !=0){
+        //         System.out.println(i);
+        //     }
+        // }
+
+        
+        //Calculate the sum of numbers from 1 to 100.
+
+        // int sum = 0;
+
+        // for( int i=1; i<=100; i++){
+
+        //     sum = sum + i;
+        // }
+        // System.out.println(sum);
+
+
+        // Calculate the sum of all even numbers from 1 to 100.
+
+        // int sum = 0;
+
+        // for ( int i=0; i<=100; i+=2){
+        //     sum = sum + i;
+        // }
+        // System.out.println(sum);
+
+
+        //Calculate the sum of all odd numbers from 1 to 100.
+
+        // int sum = 0;
+
+        // for( int i=1; i <=100; i += 2){
+            
+        //     sum = sum + i;
+        // }
+        // System.out.println(sum);
+
+
+        // Calculate the product of numbers from 1 to 5.
+
+        int product = 1;
+
+        for ( int i = 1; i <= 5; i++){
+
+            product = product * i ;
+        }
+        System.out.println(product);
+
+
+
+    }
+
+}
