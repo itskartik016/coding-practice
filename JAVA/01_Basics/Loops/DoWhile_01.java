a9df9b6d1fcc32d40ec409fd0e47061aad32b68d
Kartik Jadhav <kartikjadhav.work1@gmail.com>
2026-08-14 11:22:30 +0530

    DoWhile_1
    Implement a Do While loop that prints 'KJ' followed by the value of i.

---
+++ JAVA/01_Basics/Loops/DoWhile_01.java
@@ -0,0 +1,16 @@
+// Do While 
+import java.util.*;
+
+public class DoWhile_01 {
+    public static void main(String[] args) {
+        
+        int i = 5;
+
+        do{
+            System.out.println("KJ" + i);
+            i++;
+        } while(i <= 4);
+    }
+}
+
+// output : KJ5
