6164f834dc19c874443badbe73c36d48c4e9243a
Kartik Jadhav <kartikjadhav.work1@gmail.com>
2026-08-16 15:38:07 +0530

    Print square pattern
    This program prints a square pattern of asterisks.

---
+++ JAVA/01_Basics/Patterns/Pattern_01.java
@@ -0,0 +1,14 @@
+// Print square pattern.
+
+import java.util.*;
+
+public class Pattern_01{
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        for( int i=1; i <= 4; i++){
+            System.out.println("****");
+        }
+    }
+}
